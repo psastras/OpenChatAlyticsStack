7dd026955e14f7628cdc54e1f3013f977eb36e4f
Paul Sastrasinh <paul.sastrasinh@paulsastrashinh-mbp.local>
2016-04-10 14:54:01 -0400

    Add HealthCheck for Test

--- test/spec/spec_helper.rb
+++ test/spec/spec_helper.rb
@@ -1,4 +1,5 @@
 require 'Faraday'
+require 'waitutil'
 
 RSpec.configure do |config|
   config.color = true
@@ -12,4 +13,16 @@ def chatalytics_ui
     faraday.response :logger                  # log requests to STDOUT
     faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
   end
+end
+
+WaitUtil.wait_for_condition("OpenChatalyticsUI healthy", 
+                            :timeout_sec => 30,
+                            :delay_sec => 0.5) do 
+  begin
+    response = chatalytics_ui.get "/"
+    status = response.status
+  rescue
+    status = 0
+  end
+  status == 200
 end
